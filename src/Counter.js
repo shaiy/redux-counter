858a700bbd314293a8f57a3444c36570916969cd
Shai Yomtov <shaiy202@gmail.com>
2021-12-27 22:36:04 +0200

    action bindings + form

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { set, increment, decrement } from './actions';
+import { SetCounter } from './SetCounter';
 
 export const Counter = () => {
   const incident = 'Incident';
   const count = useSelector((state) => state.count);
   const dispatch = useDispatch();
 
+  const actions = bindActionCreators({ increment, decrement, set }, dispatch);
+
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(set(0))}>Reset</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={actions.increment}>Increment</button>
+        <button onClick={() => actions.set(0)}>Reset</button>
+        <button onClick={actions.decrement}>Decrement</button>
       </section>
+      <SetCounter />
     </main>
   );
 };
